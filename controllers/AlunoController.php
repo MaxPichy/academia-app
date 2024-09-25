6ef7f5d19e3bc5b90fcb1da86e90d7d714812be8
MaxPichy <camilamachadodesouza77@gmail.com>
2024-09-25 10:59:54 -0300

    controllers

---
+++ controllers/AlunoController.php
@@ -0,0 +1,26 @@
+<?php
+
+    class AlunoController extends Controller {
+        #Função de cadastro de aluno
+
+        public function cadastrar(){
+            $this->view("../views/aluno/cadastro");
+        }
+
+        #Função de listar aluo
+        public function listar(){
+            $alunoModel = $this->models("Aluno");
+            $alunos = $alunoModel->getAll();
+            $this->view("../views/listar", ["alunos"=>$alunos]);
+        }
+
+        #Função de registrar
+        public function registrarTreino($alunoId,$treinoId){
+            $treinoModel = $this->models("Treino");
+            $treinoModel->registrarTreino($alunoId, $treinoId);
+            header("Location: ../views/aluno/treinos/".$alunoId);
+        }
+
+    }
+
+?>
